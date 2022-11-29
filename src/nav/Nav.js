f480ebcd3db3654de99bd462d97f530c26ef9fcd
Raghavendra Diddimani <rdiddimani@mbopartners.com>
2022-11-29 23:54:11 +0530

    avatar nav item added

--- src/nav/Nav.js
+++ src/nav/Nav.js
@@ -13,6 +13,7 @@ const Nav = () => {
       <NavLink to={'/Dropdown'} key={2}className="border-b-2 border-primary-300"> Dropdown </NavLink>
       <NavLink to={'/Date'} key={3} className="border-b-2 border-primary-300"> Date </NavLink>
       <NavLink to={'/Search'} key={3} className="border-b-2 border-primary-300"> Search </NavLink>
+      <NavLink to={'/Avatar'} key={3} className="border-b-2 border-primary-300"> Avatar </NavLink>
     </ul>
     <Outlet />
   </nav>
