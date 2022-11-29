b07b080d0ca2576df1b6290bb5c893a5b530c9b5
Raghavendra Diddimani <rdiddimani@mbopartners.com>
2022-11-30 00:55:44 +0530

    search list item added

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -20,22 +20,32 @@ const Search = () => {
       {/* search__bar */}
       <div className='search__results'>
       <ul className="list">
-      <li className="list-item group" data-state="" tabIndex={0}> 
-        <div className="list-title">Title here
+      <li className="list-item" data-state="" tabIndex={1}> 
+        <div data-avatar="xl" class="avatar xl">
+            <picture className='avatar-media'>
+              <source media="(min-width:650px)" srcset="https://i.pravatar.cc/300" />
+              <source media="(min-width:465px)" srcset="https://i.pravatar.cc/300" />
+              <img src='https://i.pravatar.cc/300' alt='random profile' />
+            </picture>
+            <div className='avatar-content'>
+              <div className='avatar-title'>Camila Willians </div>
+              <div className='avatar-desc separated bullet'><span>Category</span> <span>Content Writer</span> </div>
+            </div>
           </div>
-        <div className="item-checkbox group" data-state="acctive">
-          <div className="list-label">
-          <span>A</span> -- <span>Z</span>
-          </div>
-        </div>
       </li>
-      <li className='list-divider border-secondary-400'></li>
-      <li className="list-item group" data-state="" tabIndex={1}> 
-        <div className="list-title">
-          <div className='avatar'>
-            
+      <li className='list-divider'></li>
+      <li className="list-item" data-state="" tabIndex={1}> 
+        <div data-avatar="xl" class="avatar xl">
+            <picture className='avatar-media'>
+              <source media="(min-width:650px)" srcset="https://i.pravatar.cc/300" />
+              <source media="(min-width:465px)" srcset="https://i.pravatar.cc/300" />
+              <img src='https://i.pravatar.cc/300' alt='random profile' />
+            </picture>
+            <div className='avatar-content'>
+            <div className='avatar-title'>Camila Willians </div>
+              <div className='avatar-desc separated bullet'><span>Category</span> <span>Content Writer</span> </div>
+            </div>
           </div>
-        </div>
       </li>
       
       
